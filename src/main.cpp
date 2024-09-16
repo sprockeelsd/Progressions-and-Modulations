f81d86b22cbe67541df7fc196f669580902206ec
sprockeelsd <55442124+sprockeelsd@users.noreply.github.com>
2024-09-16 11:46:18 +0200

    Diatony update
    - Pulled the Diatony version that allows to generate chromatic chords, but there are no rules enforced yet
    - Removed the post-processing on chord degrees

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,6 @@ int main(int argc, char **argv) {
     auto sol = solve_chord_progression_problem_best(size, Cmajor);
 
     auto chords = IntVarArray_to_int_vector(sol->getChords());
-    for (int i = 0; i < chords.size(); i++)
-        switch (chords[i]){ //todo handle bII somehow as well as aug6
-            case FIVE_OF_TWO:               chords[i] = SIXTH_DEGREE;       break;
-            case FIVE_OF_THREE:             chords[i] = SEVENTH_DEGREE;     break;
-            case FIVE_OF_FOUR:              chords[i] = FIRST_DEGREE;       break;
-            case FIVE_OF_FIVE:              chords[i] = SECOND_DEGREE;      break;
-            case FIVE_OF_SIX:               chords[i] = THIRD_DEGREE;       break;
-            case FIFTH_DEGREE_APPOGIATURA:  chords[i] = FIRST_DEGREE;       break;
-            default:                                                        break;
-        }
     std::cout << int_vector_to_string(chords) << std::endl;
 
     auto states = IntVarArray_to_int_vector(sol->getStates());
