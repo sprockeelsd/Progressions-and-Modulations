52fb0a8c23217d47e5cb7021f4257ce335a2a34c
sprockeelsd <55442124+sprockeelsd@users.noreply.github.com>
2025-06-03 15:47:50 +0200

    Updated Diatony version + made the link between the two
    - Added the code in the main to solve the probems of layer 2 and 3 (Diatony) together, using layer 2's output as input for layer 3

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, char **argv) {
     string four_voice = argv[1]; /// true if we want to generate the 4voice chords, false if we just want chords and state
 
+    // parameters of the layer 2 problem
     int size = 20;
     Tonality* Cminor = new MinorTonality(C);    Tonality* Ebmajor = new MajorTonality(E_FLAT);
     vector<Tonality*> tonalities = {Cminor, Ebmajor};
@@ -18,21 +19,20 @@ int main(int argc, char **argv) {
     vector<int> modulationStarts = {12};
     vector<int> modulationEnds = {13};
 
-    auto params = TonalPieceParameters(size, tonalities.size(), tonalities,
+    auto params = TonalPieceParameters(size, static_cast<int>(tonalities.size()), tonalities,
                                        modulationTypes, modulationStarts, modulationEnds);
-    std::cout << params.toString() << std::endl;
 
+    // Create an instance of the layer 2 problem (progressions and modulations)
     auto tonalPiece = new TonalPiece(&params);
 
+    // Solve layer 2 problem
     const auto sol = solve_harmoniser(tonalPiece);
     std::cout << "Best solution: \n" << sol->pretty() << std::endl;
 
-    return 0;
-
-    /// link with Diatony
+    // Create the parameters for the voicing problem
     vector<TonalProgressionParameters*> sectionParams;
-    sectionParams.reserve(tonalities.size());
     vector<ModulationParameters*> modulationParams;
+    sectionParams.reserve(tonalities.size());
     modulationParams.reserve(tonalities.size() - 1);
 
     // create the section parameters for diatony
@@ -41,33 +41,29 @@ int main(int argc, char **argv) {
         vector<int> chord_qualities = intVarArray_to_int_vector(sol->getChordProgression(i)->getQualities());
         vector<int> chord_states = intVarArray_to_int_vector(sol->getChordProgression(i)->getStates());
 
-        auto sec_params = new TonalProgressionParameters(i, sol->getChordProgression(i)->getDuration(),
+        const auto sec_params = new TonalProgressionParameters(i, sol->getChordProgression(i)->getDuration(),
                                                          sol->getChordProgression(i)->getStart(),
                                                          sol->getChordProgression(i)->getStart() + sol->getChordProgression(i)->getDuration() - 1,
                                                          tonalities[i],
                                                          chord_degrees,
                                                          chord_qualities,
                                                          chord_states);
-        std::cout << "Section " << i << " parameters: " << sec_params->to_string() << std::endl;
-        sectionParams.push_back(sec_params);
+        sectionParams.push_back(new TonalProgressionParameters(sec_params));
+        delete sec_params;
     }
 
     // create the modulation parameters
     for (int i = 0; i < modulationTypes.size(); i++) {
         auto mod_params = new ModulationParameters(modulationTypes[i], modulationStarts[i], modulationEnds[i],
                                                     sectionParams[i], sectionParams[i+1]);
-
-        std::cout << mod_params->toString() << std::endl;
-        modulationParams.push_back(mod_params);
+        modulationParams.push_back(new ModulationParameters(mod_params));
+        delete mod_params;
     }
 
-    auto pieceParams = new FourVoiceTextureParameters(size, tonalities.size(), sectionParams, modulationParams);
-    std::cout << pieceParams->toString() << std::endl;
+    // create the parameters for the four voice texture problem
+    auto pieceParams = new FourVoiceTextureParameters(size, static_cast<int>(tonalities.size()), sectionParams, modulationParams);
 
-    auto pb = new FourVoiceTexture(pieceParams);
-    std::cout << "Problem created" << std::endl;
-    std::cout << pb->to_string() << std::endl;
-
+    // Search options
     Options opts;
     opts.threads = 1;
     opts.stop = Stop::time(60000); // stop after 120 seconds
@@ -76,42 +72,19 @@ int main(int argc, char **argv) {
             Cutoff::geometric((4*pieceParams->get_totalNumberOfChords())^2, 2));
     opts.nogoods_limit = pieceParams->get_totalNumberOfChords() * 4 * 4;
 
-
-    RBS<FourVoiceTexture, BAB> solver(pb, opts);
-    delete pb;
-
-    int n_sols = 0;
-    FourVoiceTexture* lastSol = nullptr;
+    // solve the problem and measure the time taken
     auto start = std::chrono::high_resolution_clock::now();     /// start time
-    while (FourVoiceTexture* sol_fvt = solver.next()) {
-        n_sols += 1;
-        lastSol = dynamic_cast<FourVoiceTexture *>(sol_fvt->copy());
-        std::cout << sol_fvt->to_string() << std::endl;
-        std::cout << statistics_to_string(solver.statistics()) << std::endl;
-        //todo improve branching and search (see notes)
-        //if (n_sols >= 1) break;
-    }
+    auto best_sol = solve_diatony(pieceParams, &opts, true);
     auto currTime = std::chrono::high_resolution_clock::now();     /// current time
-    std::chrono::duration<double> duration = currTime - start;
+    std::chrono::duration<double> duration = currTime - start; // elapsed time
 
     std::cout << "search over" << std::endl;
-    if(solver.stopped()){
-        std::cout << "Best solution not found within the time limit." << std::endl;
-    }
-    else if(n_sols == 0){
-        std::cout << "No solutions" << std::endl;
-    }
-    else{
-        std::cout << "Best solution found." << std::endl;
-    }
-    std::cout << "time taken: " << duration.count() << " seconds and " << n_sols << " solutions found.\n" << std::endl;
 
-    if(lastSol != nullptr){
-        writeSolToMIDIFile(lastSol->getParameters()->get_totalNumberOfChords(), "out/MidiFiles/sol", lastSol);
+    if(best_sol != nullptr){
+        writeSolToMIDIFile(best_sol->getParameters()->get_totalNumberOfChords(), "out/MidiFiles/sol", best_sol);
         cout << "MIDI file(s) created" << endl;
     }
-
-    std::cout << "Summary: " << pieceParams->toString() << std::endl;
-
+    //todo summary
+    std::cout << "\n\nSummary: " << pieceParams->toString() << std::endl;
     return 0;
 }
