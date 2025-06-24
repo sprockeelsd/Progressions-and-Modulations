fd92df61153e3141356122d1dd9f9c1142fb257b
sprockeelsd <55442124+sprockeelsd@users.noreply.github.com>
2025-06-24 15:16:14 +0200

    refactor HarmoniserSolver

--- src/HarmoniserSolver.cpp
+++ src/HarmoniserSolver.cpp
@@ -4,14 +4,20 @@
 
 #include "../headers/HarmoniserSolver.hpp"
 
-TonalPiece* solve_harmoniser(TonalPiece* piece, bool print) {
+/**
+ * Solves a harmonization problem for a given TonalPiece.
+ * @param piece the TonalPiece to solve
+ * @param print if true, prints the number of solutions and the last solution found
+ * @return the last solution found, or nullptr if no solution was found
+ */
+TonalPiece* solve_harmoniser(TonalPiece* piece, const bool print) {
 
     DFS<TonalPiece> engine(piece);
     delete piece;
 
     int n_sols = 0;
     TonalPiece* last_sol = nullptr;
-    auto start = std::chrono::high_resolution_clock::now();     /// start time
+    const auto start = std::chrono::high_resolution_clock::now();     /// start time
     while(TonalPiece* sol = engine.next()) {
         last_sol = sol;
         n_sols += 1;
@@ -25,8 +31,8 @@ TonalPiece* solve_harmoniser(TonalPiece* piece, bool print) {
     if (print) std::cout << "Number of solutions: " << n_sols << std::endl <<
         "Last solution found:\n" << last_sol->pretty() << std::endl;
 
-    auto end = std::chrono::high_resolution_clock::now();     /// start time
-    std::chrono::duration<double> duration = end - start;
+    const auto end = std::chrono::high_resolution_clock::now();     /// start time
+    const std::chrono::duration<double> duration = end - start;
     if (print) std::cout << "time taken: " << duration.count() << " seconds and " << n_sols << " solutions found.\n" << std::endl;
 
     if (print) std::cout << statistics_to_string(engine.statistics());
